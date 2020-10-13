33013abaa32b3c13c20ff615bafc7bac52066fa6
Or azar <orazar1805@gmail.com>
2020-10-13 13:22:16 +0300

    Create Q3_abstract_AB.java

---
+++ Q3_abstract_AB.java
@@ -0,0 +1,31 @@
+
+public class Q3_abstract_AB {
+
+	//class A.java
+	public class A {
+		public void foo() {}
+	}
+	
+	//class B.java
+	public abstract class B {
+		public void greeting(){}
+	}
+	
+	
+	//A
+	
+	//public class test extends A,B {
+	
+	//}
+	
+	//B
+	//public class test3 implements A,B {
+		
+	//}
+	//C חסר כנראה שזאת התשובה !
+	
+	public void main(String[] args) {
+
+	}
+
+}
