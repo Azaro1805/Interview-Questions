fd5f4bb113daabb072d0d16141ade8e533d010fe
Or azar <orazar1805@gmail.com>
2020-10-15 12:18:46 +0300

    Create QQ18.java

---
+++ QQ18.java
@@ -0,0 +1,26 @@
+
+public class QQ18 extends CustomeTest {
+
+	public QQ18() {
+		super("");
+	}
+	
+}
+
+class CustomeTest{
+	private String CustomeTestName;
+	
+	public CustomeTest(String CustomeTestName) {
+		this.CustomeTestName=CustomeTestName;
+	}
+	
+	public String getCustomeTestName() {
+		return CustomeTestName;
+	}
+	
+	public void setCustomeTestName(String CustomeTestName) {
+		this.CustomeTestName=CustomeTestName;
+	}
+}
+// התשובה היא :
+// super("");
