de142b0cd283300b32fac09128a6abb2782b6844
Or azar <orazar1805@gmail.com>
2020-10-15 12:18:41 +0300

    Create QQ17.java

---
+++ QQ17.java
@@ -0,0 +1,20 @@
+
+public class QQ17 {
+
+	public static void main(String[] args) {
+		int first =100;
+		int second =200;
+	swapValues(first,second);
+	System.out.println(first);
+	System.out.println(second);
+	}
+
+	public static void swapValues(int first , int second) {
+		int temp = first;
+		first = second ;
+		second = temp;
+	}
+}
+//התשובה היא :
+//100
+//200
