4ed7a1cefcb5a4ae8bb53a4170f42f3d172c3c18
Or azar <orazar1805@gmail.com>
2020-10-15 12:18:18 +0300

    Q9

---
+++ Q9_.java
@@ -0,0 +1,18 @@
+
+public class Q9_ {
+
+	public static void main(String[] args) {
+
+	}
+
+/*
+	public int foo (int x ,int y) {
+		Integer z ;
+		if(z!=null) {
+			z=x+y;
+		}
+		return z;
+	}
+*/
+}
+// you will get compliation error !
