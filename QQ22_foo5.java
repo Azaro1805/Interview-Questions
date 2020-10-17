cbb05743a8767917b21f82259f92ba11f1c35dff
Or azar <orazar1805@gmail.com>
2020-10-17 10:35:36 +0300

    Create QQ22_foo5.java

---
+++ QQ22_foo5.java
@@ -0,0 +1,15 @@
+
+public class QQ22_foo5 {
+
+	
+	public static void main(String[] args) {
+
+		FUNCTION foo(number:integer) 
+		IFnumber==0
+			return 1
+				end if 
+				// יש המשך התשובה היא :
+				// none of above 
+	}
+
+}
